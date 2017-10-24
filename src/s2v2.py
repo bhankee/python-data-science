071b0400002d6ca9257d82d45236ff79fa78d028
Brad <brad.hankee@gmail.com>
2017-10-23 22:17:06 -0400

    Started using numpy to sort data.

---
+++ src/s2v2.py
@@ -0,0 +1,10 @@
+from s2v1 import *
+
+
+def number_of_records(data_sample):
+    return len(data_sample)
+
+
+number_of_ties = number_of_records(data_from_csv) - 1
+
+print(number_of_ties, "ties in our data sample")
